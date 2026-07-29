c381c5e53fd73b052f21cd465eb069dff1ea6f3c
Kashan Ahmed <kashanahmed3105@gmail.com>
2026-07-30 03:11:38 +0500

    .

--- server/src/app.js
+++ server/src/app.js
@@ -10,6 +10,14 @@ app.get("/", (req, res) => {
     res.send("API is running...");
 })
 
+
+app.post("/api/v1/auth/login", (req, res) => {
+    res.json({
+        success: true,
+        message: "Direct login route working"
+    });
+});
+
 app.use("/api/v1", require("./Route/authRoute"));
 app.use("/api/v1", require('./Route/employees'));
 app.use("/api/v1", require('./Route/taskRoute'));
